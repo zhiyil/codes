e96a589af0bfb369ff1429732364efed3a81e1e1
Zhiyi Liu <zhiyil@gmail.com>
2020-03-05 16:29:39 -0600

    functions for deferential analysis

---
+++ RNA-seq_data_analysis/de_funcs.R
@@ -0,0 +1,13 @@
+## differential analysis by DESeq2 ##
+
+# these are commonly used functions for plots and writing results to excel spreadsheet
+# author: Zhiyi Liu
+# version 01
+
+library(DESeq2)
+library(vsn)
+library(ggplot2)
+library(NMF)
+library(ComplexHeatmap)
+library(xlsx)
+
