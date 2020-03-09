1d0304a599467f45f72d18bacd7157f2c67461f9
Zhiyi Liu <zhiyil@gmail.com>
2020-03-09 16:10:52 -0500

    go and kegg added

--- RNA-seq_data_analysis/de_main.R
+++ RNA-seq_data_analysis/de_main.R
@@ -99,15 +99,98 @@ write.xlsx2(as.data.frame(de.results.sorted),
             col.names = T, 
             row.names = T)
 
+# go analysis - generate the matrix for go analysis
+diff.gene <- gkmatrix(de.results)
+
+############### Go enrichment analysis ###################
+library(clusterProfiler)
+library(org.Hs.eg.db)
+
+gsymbols <- diff.gene$gene_symbol
+ego <- enrichGO(gsymbols,
+                OrgDb = org.Hs.eg.db,
+                keyType = "SYMBOL",
+                ont = "ALL",
+                pAdjustMethod = "BH",
+                pvalueCutoff = 0.05,
+                qvalueCutoff = 0.1)
+
+ego_result <- as.data.frame(ego)
+
+## write the GO results into Excel
+library(xlsx)
+write.xlsx2(ego_result,
+            file = paste(compChar, " GO enrichment results on differential genes.xlsx"),
+            col.names = T, row.names = F)
 
 
+### bubble plot ####
+library(enrichplot)
+jpeg(paste(compChar, " Bubble_plot_GO_terms.jpg"), width = 1000, height = 1300)
+par(mar = plot.margin)
+dotplot(ego, showCategory = 20)
+dev.off()
+
+### network plotting ####
+plotGenes <- setNames(as.character(diff.gene$gene_symbol), 2^diff.gene$log2FoldChange) # get a vector containing fold chage values with names being the gene symbols
+
+jpeg(paste(compChar, " Network_plot_GO_terms.jpg"), width = 1000, height = 1000)
+par(mar = plot.margin)
+cnetplot(ego, categorySize = "pvalue", foldChange = plotGenes)
+dev.off()
+
+jpeg(paste(compChar, " Network_circular_plot_GO_terms.jpg"), width = 1400, height = 1200)
+par(mar = plot.margin)
+cnetplot(ego, foldChange = plotGenes, circular = T, colorEdge = T)
+dev.off()
+
+########################################################################
+## KEGG analysis ##
+
+# gene.kegg <- bitr(diff.gene$gene_symbol,
+#                   fromType = "SYMBOL",
+#                   toType = "ENTREZID",
+#                   OrgDb = org.Hs.eg.db) # use bitr to map gene symbols to entrezids
+gene.kegg <- mapIds(org.Hs.eg.db, diff.gene$gene_symbol, "ENTREZID", "SYMBOL") # here use mapIds from org.Hs.eg.db to do the mapping, !!warning!! 1:multiple mapping obtained
+
+ekg <- enrichKEGG(gene.kegg,
+                  organism = "hsa",
+                  keyType = "kegg",
+                  pvalueCutoff = 0.05,
+                  pAdjustMethod = "BH",
+                  qvalueCutoff = 0.10)
+
+ekg_result <- as.data.frame(ekg)
+
+## write the KEGG results into Excel
+library(xlsx)
+write.xlsx2(ekg_result,
+            file = paste(compChar, " KEGG enrichment results on differential genes.xlsx"),
+            col.names = T, row.names = F)
 
 
+### bubble plot ####
+library(enrichplot)
+jpeg(paste(compChar, " Bubble_plot_KEGG_terms.jpg"), width = 1000, height = 1300)
+par(mar = plot.margin)
+dotplot(ekg, showCategory = 20)
+dev.off()
 
+### network plotting ####
+plotGenes <- setNames(as.character(diff.gene$gene_symbol), 2^diff.gene$log2FoldChange) # get a vector containing fold chage values with names being the gene symbols
+ekg.symbol <- setReadable(ekg, "org.Hs.eg.db", "ENTREZID") #convert entrezid to gene symbol, making the following cnetplot readable
 
+jpeg(paste(compChar, " Network_plot_KEGG_terms.jpg"), width = 1000, height = 1000)
+par(mar = plot.margin)
+cnetplot(ekg.symbol, categorySize = "pvalue", foldChange = plotGenes)
+dev.off()
 
+jpeg(paste(compChar, " Network_circular_plot_KEGG_terms.jpg"), width = 1400, height = 1200)
+par(mar = plot.margin)
+cnetplot(ekg.symbol, foldChange = plotGenes, circular = T, colorEdge = T)
+dev.off()
 
-
+writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
 
 
 
